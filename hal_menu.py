ac3ec3ce751a579ff2b6f250f21f24f854656e67
Hilmy <hilmy_ash13@student.uns.ac.id>
2024-12-25 20:06:02 +0700

    Update hal_menu.py
    Fitur Histori

--- hal_menu.py
+++ hal_menu.py
@@ -29,11 +29,17 @@ def hal_menu(username):
         from hal_cancel import hal_cancel
         hal_menu.destroy()
         hal_cancel(username)
+    def open_histori_pesanan_user():
+        from hal_histori_user_ import hal_histori_pesanan_user
+        hal_menu.destroy()
+        hal_histori_pesanan_user(username)
 
     button_pesan = tk.Button(hal_menu, text="Pesan Tiket", width= 24, fg="#000000", bg="#C8102E", font=('Georgia', 16), command=open_hapus_film )
     button_pesan.grid(row=1, column=0)
     button_cancel = tk.Button(hal_menu, text="Cancel Tiket", width= 24, fg="#000000", bg="#C8102E", font=('Georgia', 16), command=open_histori_pesanan)
     button_cancel.grid(row=1, column=1)
+    button_histori = tk.Button(hal_menu, text="Riwayat Tiket", width= 24, fg="#000000", bg="#C8102E", font=('Georgia', 16), command=open_histori_pesanan_user)
+    button_histori.grid(row=2,column=0, columnspan=2)
 
     def open_hal_admin():
         from hal_utama import hal_Utama
